aed7aa24e18b58ec147c4eb811629a5aa7714f67
Aleksandr Motsjonov <amotsjonov@atlassian.com>
2014-10-30 18:02:16 +1100

    Fix handle of end of stream and add bunch of logging

--- tasks/selenium_server.js
+++ tasks/selenium_server.js
@@ -1,8 +1,8 @@
-
 module.exports = function (grunt) {
 
   var fs = require('fs');
   var os = require('os');
+  var util = require('util');
   var path = require('path');
   var url = require('url');
   var request = require('request');
@@ -21,53 +21,73 @@ module.exports = function (grunt) {
    * @param  {Object}   options Grunt task options.
    * @param  {Function} cb
    */
-  function downloadJar (options, cb) {
+  function downloadJar(options, cb) {
     // Where to save jar to.
     var destination = path.join(options.downloadLocation, path.basename(options.downloadUrl));
 
     // If it's already there don't download it.
     if (fs.existsSync(destination)) {
-      return cb(destination, null);
+      var stat = fs.statSync(destination);
+      var size = util.inspect(stat).size;
+      if (size > 0) {
+        grunt.log.ok("File is there with size ", size);
+        return cb(destination, null);
+      } else {
+        grunt.log.ok("File is there but it's zero size. Keeping with downloading");
+      }
     }
 
     grunt.log.ok('Saving jar to: ' + destination);
 
     var writeStream = fs.createWriteStream(destination);
 
-    // Start downloading and showing progress.
-    request(options.downloadUrl).on('response', function (res) {
-      if(res.statusCode < 200 || res.statusCode >= 400) {
-          grunt.fail.fatal(options.downloadUrl + " returns " + res.statusCode);
-      }
-      // Full length of file.
-      var len = parseInt(res.headers['content-length'], 10);
+    grunt.log.ok('Write stream is opened', !!writeStream);
 
-      // Super nifty progress bar.
-      var bar = new ProgressBar(' downloading [:bar] :percent :etas', {
-        complete: '=',
-        incomplete: ' ',
-        width: 20,
-        total: len
-      });
-
-      // Write new data to file.
-      res.on('data', function (chunk) {
-        writeStream.write(chunk);
-        bar.tick(chunk.length);
-      });
-
-      // Close file and holla back.
-      res.on('end', function () {
-        writeStream.end();
-        grunt.log.ok('done.');
-        cb(destination, null);
-      });
-
-      // Download error.
-      res.on('error', function (err) {
-        cb(null, err);
-      });
+    writeStream.on('close', function(){
+      grunt.log.ok("Write stream is closed now");
+      cb(destination, null);
     });
+
+    writeStream.on('write', function(chunk){
+      grunt.log.ok("Chunk");
+    });
+
+    writeStream.on('error', function(error){
+      grunt.log.error("There was an error writing to a file", error);
+      cb(null, error);
+    })
+
+    // Start downloading and showing progress.
+    request(options.downloadUrl)
+        .on('response', function (res) {
+          grunt.log.ok("We have a response");
+          if (res.statusCode < 200 || res.statusCode >= 400) {
+            grunt.fail.fatal(options.downloadUrl + " returns " + res.statusCode);
+          }
+          // Full length of file.
+          var len = parseInt(res.headers['content-length'], 10);
+          grunt.log.ok("Response length is", len, "bytes");
+
+          // Super nifty progress bar.
+          var bar = new ProgressBar(' downloading [:bar] :percent :etas', {
+            complete: '=',
+            incomplete: ' ',
+            width: 20,
+            total: len
+          });
+
+          // Write new data to file.
+          res.on('data', function (chunk) {
+            bar.tick(chunk.length);
+          });
+
+          // Download error.
+          res.on('error', function (err) {
+            grunt.log.error("Something happened with response");
+            cb(null, err);
+          });
+        })
+        .pipe(writeStream);
   }
 
   /**
@@ -78,7 +98,7 @@ module.exports = function (grunt) {
    * @param  {Object}   options Grunt task options.
    * @param  {Function} cb
    */
-  function startServer (target, jar, options, cb) {
+  function startServer(target, jar, options, cb) {
     var args = ['-jar', jar];
 
     // Add additional options to command.
@@ -92,6 +112,7 @@ module.exports = function (grunt) {
     });
 
     grunt.log.ok('Starting Selenium server...');
+    grunt.verbose.writeflags(args, "java arguments");
 
     // Spawn server process.
     grunt.log.ok('Using (roughly) command: java ' + args.join(' '));
@@ -105,27 +126,30 @@ module.exports = function (grunt) {
 
     var complete = false;
 
-    childProcesses[target].stdout.on('data', function(data) {
+    childProcesses[target].stdout.on('data', function (data) {
+      grunt.log.ok("Selenium Sever output:", data);
       if (data.toString().match(/Started SocketListener on .+:\d+/)) {
         if (complete) return;
         grunt.log.ok('Selenium server SocketListener started.');
 
         // Wait a tiny bit more time just because it's java and I'm worried.
-        setTimeout(function(){
+        setTimeout(function () {
+          grunt.log.ok("After two seconds from selenium start");
           complete = true;
           cb(null);
         }, 2000);
       }
     });
 
-    if(options.showErrors || options.showErrors == undefined){
-      childProcesses[target].stderr.on('data', function(data) {
+    if (options.showErrors || options.showErrors == undefined) {
+      childProcesses[target].stderr.on('data', function (data) {
         grunt.log.error(data.toString());
+        cb(new Error(data));
       });
     }
 
     // Timeout case
-    setTimeout(function() {
+    setTimeout(function () {
       if (!complete) {
         complete = true;
         // Try to clean up better after ourselves
@@ -140,6 +164,10 @@ module.exports = function (grunt) {
    */
   grunt.registerMultiTask('start-selenium-server', 'Start Selenium server.', function () {
     var done = this.async();
+    var loggedDone = function () {
+      grunt.log.ok("Task is done.");
+      done();
+    };
     var target = this.target;
 
     // Set default options.
@@ -154,19 +182,21 @@ module.exports = function (grunt) {
 
     // Download jar file. Doesn't do anything if the file's already been downloaded.
     downloadJar(options, function (jar, err) {
+      grunt.log.ok("Inside download callack");
       if (err) {
         grunt.log.error(err);
-        return done(false);
+        return loggedDone(false);
       }
 
       // Start the selenium server in a child process.
       startServer(target, jar, options, function (err) {
+        grunt.log.ok("Inside start server callback");
         if (err) {
           grunt.log.error(err);
-          return done(false);
+          return loggedDone(false);
         }
 
-        done(true);
+        loggedDone(true);
       });
     });
   });
